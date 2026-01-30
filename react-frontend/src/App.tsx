f62b0e3f7afe778e9e2bb722aa350ac64bc3c97e
Juan Carlos Orcko <juancarlosorcko78@gmail.com>
2026-01-30 15:34:13 -0400

    Add Logout component to frontend dashboard

--- react-frontend/src/App.tsx
+++ react-frontend/src/App.tsx
@@ -1,19 +1,32 @@
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
 import './App.css'
 import Profile from './pages/Profile'
 import Register from './pages/Register'
 import Dashboard from './pages/Dashboard'
 import Login from './pages/Login'
+import { useEffect, useState } from 'react'
 
 function App() {
+
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const location = useLocation();
+
+  useEffect(() => {
+    const auth = localStorage.getItem('isAuthenticated');
+    setIsAuthenticated(auth === 'true');
+  }, [location]);
 
   return (
     <>
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        {isAuthenticated && (
+          <>
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+          </>
+        )}
       </Routes>
     </>
   )
